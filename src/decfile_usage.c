ba034b20c839d1c61dce5de42e75d9bc3f6ed313
YourFritz <git@yourfritz.de>
2017-06-15 15:13:05 +0200

    help screen for 'decode_secrets' was incomplete

--- src/decfile_usage.c
+++ src/decfile_usage.c
@@ -70,53 +70,52 @@ void 	decfile_usage(const bool help, UNUSED const bool version)
 	showOptionsEnd(out);
 
 	fprintf(out,
-		"\nThe %s has to be a saved-settings file, which was exported from FRITZ!OS.\n",
+		"\nThe %s may be any text file, starting from a real TFFS node up to a single line of Base32\n"
+		"encoded cipher-text, as long as any encrypted value starts with the four dollar-signs. This can be\n"
+		"useful, if you extract some settings from a configuration file with other utilities (e.g.\n"
+		"'ar7cfgctl') and want to decode only a single value or a smaller amount of values.\n",
 		showUndl("input-file")
 	);
 
 	fprintf(out,
-		"\nThese files use a two-stage encryption, where a random value will be chosen and encrypted with the\n"
-		"key for the first stage. Then it will be stored in an entry with the name 'Password' in the file\n"
-		"header. All further data inside the file will be encrypted with this random key. The first key's\n"
-		"value is derived from device properties (see %s for details), if an export file\n"
-		"is created without an user-specified %s. Otherwise this %s will be used to compute the\n"
-		"first key and you must specify the same %s to decrypt the file.\n",
-		showBold(DECODER_CONFIG_KEYS_CURRENT_DEVICE_NAME),
-		showUndl("password"), showUndl("password"), showUndl("password")
+		"\nSTDIN has to be redirected or the program will be aborted - it's a protection against accidental\n"
+		"calls without proper redirection, otherwise the program would block the caller. If you really want\n"
+		"to call it with STDIN connected to a terminal device, specify the '--tty' option for the call.\n\n"
+		"If STDIN is connected to a terminal device and the '--tty' option (or '-t') wasn't specified, the\n"
+		"last expected value from command line is checked, wether it's the name of a readable file. In this\n"
+		"case the specified %s will be used as source of input data.\n",
+		showUndl("input-file")
 	);
 
 	fprintf(out,
-		"\nIf a file was created without a password, there are two alternatives to specify the two needed\n"
-		"properties of the source device. You can use the second form of parameters with %s and %s set\n"
-		"to the correct values or you can use an alternative file for the 'urlader environment' (using the\n"
-		"'--alt-env' (or '-a') option from above) and the program will extract the values there.\n"
-		"\nAn alternative environment file is ignored, if the first format with %s is used.\n",
-		showUndl("serial"), showUndl("maca"), showUndl("password")
+		"\nThe number of %ss depends on the mode of the call. If no %s was specified, the\n"
+		"decryption key is built from device properties in the 'urlader environment'. If the program isn't\n"
+		"called on a FRITZ!OS device, the 'urlader environment' may be substituted by a text file. Its\n"
+		"location can be specified with the '--alt-env' (or '-a') option.\n",
+		showUndl("parameter"), showUndl("parameter")
 	);
 
 	fprintf(out,
-		"\nIf the key value for the second stage can not be decrypted, further processing will be aborted.\n"
+		"\nIf a single %s is used, it has to be a hexadecimal string representing the key to be used\n"
+		"for decryption.\n",
+		showUndl("parameter")
 	);
 
 	fprintf(out,
-		"\nAll input data will be read from STDIN; you can provide it using shell redirections. If STDIN is\n"
-		"connected to a terminal device and the '--tty' option (or '-t') wasn't specified, the last value\n"
-		"from command line (following %s or %s and %s values) is checked, wether it's the name\n"
-		"of a readable file. In this case the specified %s will be used as source of input data.\n",
-		showUndl("password"), showUndl("serial"), showUndl("maca"), showUndl("input-file")
+		"\nIf more than one %s exists, it's an alternative approach to decrypt data from another\n"
+		"FRITZ!OS device with knowledge of its properties - this one doesn't need an environment file\n"
+		"from somewhere. For this mode, you need at least the '%s', '%s' and '%s' values\n"
+		"of the foreign device. If it has a pre-configured CWMP account, you need the value from the field\n"
+		"'%s' of the device too.\n",
+		showUndl("parameter"), showUndl(URLADER_SERIAL_NAME), showUndl(URLADER_MACA_NAME), 
+		showUndl(URLADER_WLANKEY_NAME), showUndl(URLADER_TR069PP_NAME)
 	);
 
 	fprintf(out,
-		"\nIf the '--tty' option wasn't specified, STDIN is a terminal device and the last argument isn't\n"
-		"the name of a readable file, then the execution will be aborted.\n"
-	);
-
-	fprintf(out,
-		"\nThe encrypted value of the 'Password' field in the header will remain unchanged. Its value isn't\n"
-		"usable for other cases and as long as it's kept intact, the file may be later imported again by a\n"
-		"FRITZ!OS device (with the right properties or with a password), even if all encrypted values are\n"
-		"replaced by their corresponding cleartext. Only the CRC32 checksum at the end of the file has to be\n"
-		"re-computed.\n"
+		"\nIf you've got a copy of the 'urlader environment' for the device, where your %s was\n"
+		"created, you should use this file with the '--alt-env' option instead of specifying three and more\n"
+		"values for the call.\n",
+		showUndl("input-file")
 	);
 
 	showUsageFinalize(out, help, version);
