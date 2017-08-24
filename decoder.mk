428de17b93d78ba460f58f545225cb748254f269
YourFritz <git@yourfritz.de>
2017-08-24 22:39:57 +0200

    Freetz toolchain integration

--- decoder.mk
+++ decoder.mk
@@ -3,33 +3,19 @@ $(call PKG_INIT_BIN, v0.3)
 $(PKG)_BINARY:=$($(PKG)_DIR)/decoder
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/bin/decoder
 
-$(PKG)_DEPENDS_ON += openssl
-
-ifeq ($(strip $(FREETZ_PACKAGE_DECODER_STATIC)),y)
-$(PKG)_LINK_STATIC = 1
-override $(PKG)_LIBS = -L$(DECODER_DIR) -lcrypto -lgcc_s
-endif
-
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_DECODER_STATIC
-
 $(PKG_LOCALSOURCE_PACKAGE)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	ln -sf $(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libgcc_pic.a $(DECODER_DIR)/libgcc_s.a
 	$(SUBMAKE) -C $(DECODER_DIR) \
-		STATIC=$(DECODER_LINK_STATIC) \
 		CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS)" \
-		LIBS="$(DECODER_LIBS)" \
-		OPT=""
+		OPT="" \
+		OPENSSL=$(if $(FREETZ_PACKAGE_DECODER_OPENSSL),y,n) \
+		STATIC=$(if $(FREETZ_PACKAGE_DECODER_DYNAMIC),n,y)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_BINARY_STRIP)
-	$(SUBMAKE) -C $(DECODER_DIR) \
-		INSTALLDIR="$(FREETZ_BASE_DIR)/$(dir $@)" \
-		SYMLINK_TARGETS="$(strip $(DECODER_SYMLINKS))" \
-		install-links
 
 $(pkg):
 
@@ -40,8 +26,7 @@ $(pkg)-clean:
 
 $(pkg)-uninstall:
 	$(SUBMAKE) -C $(DECODER_DIR) \
-		INSTALLDIR="$(dir $@)" \
-		SYMLINK_TARGETS="$(DECODER_SYMLINKS)" \
+		bindir="$(dir $@)" \
 		uninstall
 
 $(PKG_FINISH)
